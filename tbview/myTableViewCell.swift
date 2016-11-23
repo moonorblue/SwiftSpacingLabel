9270bb19212882fb1e09ccbd41daf9dd80cb4c3e
KaeJer Cho <kaejercho@KaeJers-Mac-mini.local>
2016-11-23 22:31:38 +0800

    first commit

---
+++ tbview/myTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  myTableViewCell.swift
+//  tbview
+//
+//  Created by KaeJer Cho on 2016/11/23.
+//  Copyright © 2016年 KaeJer Cho. All rights reserved.
+//
+
+import UIKit
+
+class myTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
